dca67550fb1401b6ab440e84c8b8bdb953bb1957
diak <diak@10.9.8.12>
2019-02-14 08:01:22 +0100

    fuggveny athelyezese a fgv.php-be

--- index.php
+++ index.php
@@ -7,18 +7,11 @@
     <body>
         Ez az index.php
         <?php
+           
+            $sorsolt= sorsolas();
+            var_dump($sorsolt);
 			//lottohuzas
-                        $kihuzottak=0;
-                        while($kihuzottak<5)
-                        {
-                            $sorsoltszam=rand(1,90);
-                            if(!isset($tomb[$sorsoltszam]))
-                            {
-                                $tomb[$sorsoltszam]=1;
-                                $kihuzottak++;
-                            }
-                        }
-                        var_dump($tomb);
+               
                          echo "ez msr s szam tomb!";
             echo "itt mar a php script fut";
         // put your code here
